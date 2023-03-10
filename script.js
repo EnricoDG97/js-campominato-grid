a3f4d40a56d84d0422e680b8da0bbe27a5fb0747
EnricoDG97 <enrico.delgaudio97@gmail.com>
2023-03-10 17:58:06 +0100

    feat: added milestone 2, bonus difficolty levels and select square, added style with css

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ Consegna
 1. L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
 2. Ogni cella ha un numero progressivo, da 1 a 100.
 Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+Quando l'utente clicca su ogni cella, la cella cliccata si colora di GIALLO ed emetto un messaggio in console con il numero della cella cliccata.
 
 Bonus
 Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
@@ -15,13 +15,51 @@ Aggiungere una select accanto al bottone di generazione, che fornisca una scelta
 
 const playButtonEl = document.getElementById("play");
 const gridContainerEl = document.getElementById("gridContainer");
+const diffLevelEL = document.getElementById("level");
 
+    
+// creo evento al pulsante per iniziare il gioco
 playButtonEl.addEventListener("click", function () {
     gridContainerEl.innerHTML = "";
-    const squareEl = document.createElement("div");
-    squareEl.style.width = "20px";
-    squareEl.style.height = "20px";
+    const level = diffLevelEL.value;
+    const gridElement = document.createElement("div");
+    gridElement.classList.add("grid");
+    const squareNumber = getSquareNumber(level);
+    const columnNumber = Math.sqrt(squareNumber);
+    createGrid(gridElement, squareNumber, columnNumber);
+    gridContainerEl.append(gridElement);
+}); 
 
-    squareEl.classList.add("square");
-    gridContainerEl.append(squareEl);
-});
+function createGrid(gridElement, squareNumber, columnNumber) {
+    for (let i = 1; i <= squareNumber; i++) {
+        const squareEl = createSquare(i, columnNumber);
+        // alle pressione di ogni cella:
+        squareEl.addEventListener('click', function () {
+        squareEl.classList.toggle('selected');
+        console.log(i);
+    });
+        gridElement.append(squareEl);
+    }
+  }
+
+// creao funzione per generare i quadrati dimensionati in base al numero delle colonne che voglio
+function createSquare(squareNumber, columnNumber) {
+    const squareEl = document.createElement('div');
+    // stilizzazione
+    squareEl.style.width = `calc(100% / ${columnNumber})`;
+    squareEl.style.height = `calc(100% / ${columnNumber})`;
+    squareEl.classList.add('square');
+    squareEl.textContent = squareNumber;
+    return squareEl; 
+}
+
+// creo funzione per il numero dei quadrati in base al livello di diffcoltà
+function getSquareNumber(level) {
+    if (level == 1) {
+        return 100;
+    } else if (level == 2) {
+        return 81;
+    } else {
+        return 49;
+    }
+}
